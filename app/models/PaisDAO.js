07b94ad766b5f72faf89d1fa7e8f728f7972c778
EduardoGimenesCustodio <eduardo.gimenes.custodio@gmail.com>
2019-10-09 18:20:20 -0400

    Tela páis exibindo alertas

--- app/models/PaisDAO.js
+++ app/models/PaisDAO.js
@@ -31,11 +31,15 @@ function PaisDAO(connection){
 	}
 
 	PaisDAO.prototype.getMoedasPais = function(nome_pais, callback){
-		this._connection.query('select nome_moeda from moeda where id_moeda in (select moeda_moeda_pais from moeda_pais where pais_moeda_pais = (select id_pais from pais where nome_pais = "' + nome_pais.nome_pais + '"))', callback)
+		this._connection.query('select nome_moeda from moeda where id_moeda in (select moeda_moeda_pais from moeda_pais where pais_moeda_pais = (select id_pais from pais where nome_pais = "' + nome_pais.nome_pais + '"))', callback);
 	}
 
 	PaisDAO.prototype.getLinguasPais = function(nome_pais, callback){
-		this._connection.query('select nome_lingua from lingua where id_lingua in (select lingua_lingua_pais from lingua_pais where pais_lingua_pais = (select id_pais from pais where nome_pais = "' + nome_pais.nome_pais + '"))', callback)
+		this._connection.query('select nome_lingua from lingua where id_lingua in (select lingua_lingua_pais from lingua_pais where pais_lingua_pais = (select id_pais from pais where nome_pais = "' + nome_pais.nome_pais + '"))', callback);
+	}
+
+	PaisDAO.prototype.getAlertasPais = function(nome_pais, callback){
+		this._connection.query('select titulo_alerta, date_format(data_alerta, "%d/%m/%Y") as data_alerta, descricao_alerta from alerta where id_alerta in (select alerta_alerta_pais from alerta_pais where pais_alerta_pais = (select id_pais from pais where nome_pais = "' + nome_pais.nome_pais + '")) order by(data_alerta) desc', callback);
 	}
 
 	PaisDAO.prototype.buscarPais = function(pais_busca, callback){
